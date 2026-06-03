07bcf8925f8c322a644de1f7c36dee02cd96b824
daixgregory <daixgregory@gmail.com>
2026-06-03 12:23:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
+function addTask(){
 
+const input = document.getElementById("taskInput");
+
+if(input.value.trim() === ""){
+return;
+}
+
+const list = document.getElementById("missionList");
+
+const li = document.createElement("li");
+
+li.innerHTML = "🟢 " + input.value;
+
+list.appendChild(li);
+
+saveTasks();
+
+input.value = "";
+
+}
+
+function saveTasks(){
+
+localStorage.setItem(
+"jarvis_tasks",
+document.getElementById("missionList").innerHTML
+);
+
+}
+
+function loadTasks(){
+
+const saved = localStorage.getItem("jarvis_tasks");
+
+if(saved){
+document.getElementById("missionList").innerHTML = saved;
+}
+
+}
+
+loadTasks();
