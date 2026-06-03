991a888a968afd06fc9742adb8713e083deed94e
daixgregory <daixgregory@gmail.com>
2026-06-03 13:00:49 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+function openModule(name){
+
+alert("Module " + name + " en développement");
+
+}
+
 function addTask(){
 
 const input = document.getElementById("taskInput");
@@ -14,48 +20,6 @@ li.innerHTML = "🟢 " + input.value;
 
 list.appendChild(li);
 
-saveTasks();
-
 input.value = "";
-
-}
-
-function saveTasks(){
-
-localStorage.setItem(
-"jarvis_tasks",
-document.getElementById("missionList").innerHTML
-);
-
-}
-
-function loadTasks(){
-
-const saved = localStorage.getItem("jarvis_tasks");
-
-if(saved){
-document.getElementById("missionList").innerHTML = saved;
-}
-
-}
-
-loadTasks();
-function openModule(module){
-
-if(module === "sport"){
-alert("Module SPORT en développement");
-}
-
-if(module === "invest"){
-alert("Module INVESTISSEMENTS en développement");
-}
-
-if(module === "mecano"){
-alert("Module MÉCANIQUE en développement");
-}
-
-if(module === "urbanisme"){
-alert("Module URBANISME en développement");
-}
 
 }
